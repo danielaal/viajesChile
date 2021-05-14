612705813d45a3e5ff426c02508acb06a5dd3b17
danielaal <69656746+danielaal@users.noreply.github.com>
2021-05-13 21:20:59 -0400

    first commit

---
+++ assets/Js/script.js
@@ -0,0 +1,25 @@
+$(function(){
+
+    $('[data-toggle="tooltip"]').tooltip();
+
+    $( ".card_title1").click(function() {
+        $(".card-text1" ).toggle(  function() {
+        });
+    });
+
+    $( ".card_title2" ).click(function() {
+        $(".card-text2" ).toggle(  function() {
+        });
+    });
+
+    $( ".card_title3" ).click(function() {
+        $(".card-text3" ).toggle(  function() {
+        });
+    });
+
+    $( ".card_title4" ).click(function() {
+        $(".card-text4" ).toggle(  function() {
+        });
+    });
+
+)} 
